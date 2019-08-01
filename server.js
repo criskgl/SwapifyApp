d14c16cdaeb8866f3e734ae59059b27c726208c0
Cris <cris.kgl@gmail.com>
2019-08-01 12:57:32 +0000

    fixing redirect uris

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ var client_secret = "32ab56c55e5640b0b3a4855a0959f251"; // Your secret
  * for deploying localy in localhost:8888 just comment the seond
  * delaration of redirect uri
  */
-var redirect_uri = "http://localhost:8888/home"; // Your redirect uri
+var origin = location.origin;
+var redirect_uri = origin + "/home"; // Your redirect uri
 //var redirect_uri =  'https://swapify123.herokuapp.com/home';
 
 console.log(redirect_uri);
@@ -26,7 +27,7 @@ console.log(redirect_uri);
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
-var generateRandomString = function(length) {
+var generateRandomString = function (length) {
   var text = "";
   var possible =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -49,7 +50,7 @@ app
 app.get(
   "/login",
 
-  function(req, res) {
+  function (req, res) {
     var state = generateRandomString(16);
     res.cookie(stateKey, state);
 
@@ -59,18 +60,18 @@ app.get(
       "user-read-private user-read-email playlist-modify-public playlist-modify-private";
     res.redirect(
       "https://accounts.spotify.com/authorize?" +
-        querystring.stringify({
-          response_type: "code",
-          client_id: client_id,
-          scope: scope,
-          redirect_uri: redirect_uri,
-          state: state
-        })
+      querystring.stringify({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state
+      })
     );
   }
 );
 
-app.get("/home", function(req, res) {
+app.get("/home", function (req, res) {
   // your application requests refresh and access tokens
   // after checking the state parameter
   var code = req.query.code || null;
@@ -80,9 +81,9 @@ app.get("/home", function(req, res) {
   if (state === null || state !== storedState) {
     res.redirect(
       "/#" +
-        querystring.stringify({
-          error: "state_mismatch"
-        })
+      querystring.stringify({
+        error: "state_mismatch"
+      })
     );
   } else {
     res.clearCookie(stateKey);
@@ -101,7 +102,7 @@ app.get("/home", function(req, res) {
       json: true
     };
 
-    request.post(authOptions, function(error, response, body) {
+    request.post(authOptions, function (error, response, body) {
       if (!error && response.statusCode === 200) {
         var access_token = body.access_token,
           refresh_token = body.refresh_token;
@@ -113,31 +114,31 @@ app.get("/home", function(req, res) {
         };
 
         // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
+        request.get(options, function (error, response, body) {
           console.log(body);
         });
 
         // we can also pass the token to the browser to make requests from there
         res.redirect(
           "/homeUser/index.html?" +
-            querystring.stringify({
-              access_token: access_token,
-              refresh_token: refresh_token
-            })
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          })
         );
       } else {
         res.redirect(
           "/#" +
-            querystring.stringify({
-              error: "invalid_token"
-            })
+          querystring.stringify({
+            error: "invalid_token"
+          })
         );
       }
     });
   }
 });
 
-app.get("/refresh_token", function(req, res) {
+app.get("/refresh_token", function (req, res) {
   // requesting access token from refresh token
   var refresh_token = req.query.refresh_token;
   var authOptions = {
@@ -154,7 +155,7 @@ app.get("/refresh_token", function(req, res) {
     json: true
   };
 
-  request.post(authOptions, function(error, response, body) {
+  request.post(authOptions, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token;
       res.send({
